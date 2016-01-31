b7d62d66322942efd31fb1598e23874cad143460
Denis Defreyne <denis.defreyne@stoneship.org>
2016-01-31 10:44:59 +0100

    Parse and translate

--- dmark.rb
+++ dmark.rb
@@ -1,15 +1,141 @@
-def translate_elem_name(name)
-  case name
-  when 'listing'
-    'pre'
-  when 'firstterm', 'identifier', 'glob', 'emph', 'filename', 'class'
-    'i'
-  when 'command'
-    'code'
-  when 'p', 'dl', 'dt', 'dd', 'code', 'h1', 'h2', 'h3', 'ul', 'li'
-    name
-  else
-    raise "Cannot translate #{name}"
+class MyHTMLTranslator
+  def initialize(tree)
+    @tree = tree
+  end
+
+  def run
+    ''.tap { |io| handle(@tree, io) }
+  end
+
+  private
+
+  def handle(node, io)
+    case node
+    when RootNode
+      node.children.each { |child| handle(child, io) }
+    when TextNode
+      io << node.text
+    when ElementNode
+      io << "<#{translate_elem_name(node.name)}>"
+      node.children.each { |child| handle(child, io) }
+      io << "</#{translate_elem_name(node.name)}>"
+    end
+  end
+
+  def translate_elem_name(name)
+    case name
+    when 'listing'
+      'pre'
+    when 'firstterm', 'identifier', 'glob', 'emph', 'filename', 'class'
+      'i'
+    when 'command'
+      'code'
+    when 'p', 'dl', 'dt', 'dd', 'code', 'h1', 'h2', 'h3', 'ul', 'li'
+      name
+    else
+      raise "Cannot translate #{name}"
+    end
+  end
+end
+
+#########################
+
+class Node
+  attr_reader :children
+
+  def initialize
+    @children = []
+  end
+
+  def inspect(indent = 0)
+    'Node()'
+  end
+end
+
+class RootNode < Node
+  def inspect(indent = 0)
+    io = ''
+    io << '  ' * indent
+    io << 'Root('
+    io << "\n" if children.any?
+    children.each { |c| io << c.inspect(indent + 1) }
+    io << '  ' * indent if children.any?
+    io << ')'
+    io << "\n"
+    io
+  end
+end
+
+class TextNode < Node
+  attr_reader :text
+
+  def initialize(text:)
+    super()
+    @text = text
+  end
+
+  def inspect(indent = 0)
+    io = ''
+    io << '  ' * indent
+    io << 'Text('
+    io << @text.inspect
+    io << "\n" if children.any?
+    children.each { |c| io << c.inspect(indent + 1) }
+    io << '  ' * indent if children.any?
+    io << ')'
+    io << "\n"
+    io
+  end
+end
+
+class ElementNode < Node
+  attr_reader :name
+
+  def initialize(name:)
+    super()
+    @name = name
+  end
+
+  def inspect(indent = 0)
+    io = ''
+    io << '  ' * indent
+    io << 'Element('
+    io << @name
+    io << "\n" if children.any?
+    children.each { |c| io << c.inspect(indent + 1) }
+    io << '  ' * indent if children.any?
+    io << ')'
+    io << "\n"
+    io
+  end
+end
+
+#########################
+
+class Parser
+  def initialize(tokens)
+    @tokens = tokens
+
+    @root_node = RootNode.new
+  end
+
+  def run
+    node_stack = [@root_node]
+
+    @tokens.each do |token|
+      case token
+      when TextToken
+        node_stack.last.children << TextNode.new(text: token.text)
+      when TagBeginToken
+        new_node = ElementNode.new(name: token.name)
+        node_stack.last.children << new_node
+        node_stack.push(new_node)
+      when TagEndToken
+        node_stack.pop
+      end
+    end
+
+    @root_node
   end
 end
 
@@ -29,7 +155,7 @@ class TextToken < Token
   end
 
   def to_s
-    @text
+    "Text(#{@text.inspect})"
   end
 end
 
@@ -43,13 +169,13 @@ end
 
 class TagBeginToken < AbstractTagToken
   def to_s
-    "<#{translate_elem_name(name)}>"
+    "TagBegin(#{name.inspect})"
   end
 end
 
 class TagEndToken < AbstractTagToken
   def to_s
-    "</#{translate_elem_name(name)}>"
+    "TagEnd(#{name.inspect})"
   end
 end
 
@@ -192,5 +318,9 @@ class Lexer
   end
 end
 
+#########################
+
 tokens = Lexer.new(File.read(ARGV[0])).run
-puts tokens.join('')
+tree = Parser.new(tokens).run
+translator = MyHTMLTranslator.new(tree)
+puts translator.run
