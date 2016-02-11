0d0078363399bde25c3f66ea59c3fd5e251c0683
Denis Defreyne <denis.defreyne@stoneship.org>
2016-02-11 22:21:45 +0100

    Add translator

--- lib/d-mark.rb
+++ lib/d-mark.rb
@@ -1,1 +1,2 @@
 require_relative 'd-mark/parser'
+require_relative 'd-mark/translator'
---
+++ samples/trivial.rb
@@ -0,0 +1,20 @@
+require 'd-mark'
+
+class MyHTMLTranslator < DMark::Translator
+  def handle(node)
+    case node
+    when String
+      out << node
+    when DMark::Parser::ElementNode
+      out << "<#{node.name}>"
+      handle_children(node)
+      out << "</#{node.name}>"
+    end
+  end
+end
+
+content = File.read('samples/trivial.dmark')
+parser = DMark::Parser.new(content)
+tree = parser.parse
+
+puts MyHTMLTranslator.new(tree).run
