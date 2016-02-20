d19c375f0ad7a9824629d97a885a236b6dcaf3ed
Denis Defreyne <denis.defreyne@stoneship.org>
2016-02-20 10:20:15 +0100

    Test ElementNode#==

--- spec/d-mark/element_node_spec.rb
+++ spec/d-mark/element_node_spec.rb
@@ -20,4 +20,35 @@ describe DMark::ElementNode do
       it { is_expected.to eql('Element(para, {"only"=>"web"}, ["Hello!"])') }
     end
   end
+
+  describe '#==' do
+    subject { element_node == other }
+
+    context 'other is not an element node' do
+      let(:other) { 'donkey' }
+      it { is_expected.to be false }
+    end
+
+    context 'other is an element node' do
+      context 'other does not differ' do
+        let(:other) { described_class.new(name, attributes, children) }
+        it { is_expected.to be true }
+      end
+
+      context 'other differs in name' do
+        let(:other) { described_class.new('giraffe', attributes, children) }
+        it { is_expected.to be false }
+      end
+
+      context 'other differs in attributes' do
+        let(:other) { described_class.new(name, { 'friend' => 'donkey' }, children) }
+        it { is_expected.to be false }
+      end
+
+      context 'other differs in children' do
+        let(:other) { described_class.new(name, attributes, []) }
+        it { is_expected.to be false }
+      end
+    end
+  end
 end
