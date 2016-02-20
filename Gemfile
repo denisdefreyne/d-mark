9d612dd2f7f24e06d90b60e7c3fc70f984075900
Denis Defreyne <denis.defreyne@stoneship.org>
2016-02-20 09:53:12 +0100

    Set up Codecov

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 
 gemspec
 
+gem 'codecov', require: false
 gem 'guard'
 gem 'guard-rake'
 gem 'rspec'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,10 @@
 require 'rspec'
+
+require 'simplecov'
+SimpleCov.start
+if ENV['CI'] == 'true'
+  require 'codecov'
+  SimpleCov.formatter = SimpleCov::Formatter::Codecov
+end
+
 require 'd-mark'
